36eb121fd65e7a362870809f4e1226a7bafc931d
jameshaverstick <jhaverstick99@gmail.com>
2021-10-04 16:58:04 -0400

    Moved distance function into outlier_removal.py

--- spectraframe/tools/outlier_removal.py
+++ spectraframe/tools/outlier_removal.py
@@ -1,5 +1,13 @@
 import numpy as np
-from ._outlier_removal_functions import distance
+
+def distance(a, b):
+    """
+    Return distance between two arrays.
+    :param a: array 1
+    :param b: array 2
+    :return: distance between two arrays.
+    """
+    return np.sqrt(np.sum(np.square(a - b)))
 
 
 def pick_outliers(sdf, N, region=None, method='max'):
