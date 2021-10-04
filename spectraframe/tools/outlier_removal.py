468e722e037b5c7cb16b736dea6228102667ba57
jameshaverstick <jhaverstick99@gmail.com>
2021-10-04 16:56:15 -0400

    outlier removal method docstring update

--- spectraframe/tools/outlier_removal.py
+++ spectraframe/tools/outlier_removal.py
@@ -2,12 +2,13 @@ import numpy as np
 from ._outlier_removal_functions import distance
 
 
-def pick_outliers(sdf, N, region=None, method='distance'):
+def pick_outliers(sdf, N, region=None, method='max'):
     """
     Return N outliers sorted by distance to avg over given region.
     :param sdf: SpectraDataFrame
     :param N: Number of outliers to return
     :param region: Region to confine measurement.
+    :param method: Method to choose outliers by.
     :return:
     """
     new_sdf = sdf.copy()
@@ -15,11 +16,11 @@ def pick_outliers(sdf, N, region=None, method='distance'):
         new_sdf.crop(region[0], region[1])
     avg = new_sdf.mean()
     if method == 'distance':
-        distances = [distance(avg, np.array(new_sdf[i])) for i in new_sdf]
+        values = [distance(avg, np.array(new_sdf[i])) for i in new_sdf]
     elif method == 'max':
-        distances = [max(np.abs(avg - np.array(new_sdf[i]))) for i in new_sdf]
+        values = [max(np.abs(avg - np.array(new_sdf[i]))) for i in new_sdf]
     else:
-        raise ValueError('Unknown method')
-    sortedspectra = [x for _, x in sorted(zip(distances, sdf.names))]
+        raise ValueError('Unknown method.')
+    sortedspectra = [x for _, x in sorted(zip(values, sdf.names))]
     sortedspectra.reverse()
     return sortedspectra[:N]
