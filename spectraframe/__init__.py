7434ef86c24a28938cdca8d928859cdb1cbae932
jameshaverstick <jhaverstick99@gmail.com>
2021-10-04 17:20:00 -0400

    Added interpolation

--- spectraframe/__init__.py
+++ spectraframe/__init__.py
@@ -2,3 +2,4 @@ from .spectradataframe.spectradataframe import SpectraDataFrame
 from .plotting.plottingtools import plot
 from .spectradataframe.importing import read_csv, read_tsv
 from .tools.outlier_removal import pick_outliers
+from .tools.interpolation import interpolate
