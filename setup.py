ad09c943ceed6a89ae8e3a30c94385494426f981
jameshaverstick <jhaverstick99@gmail.com>
2021-10-06 17:49:49 -0400

    Adjusted setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,13 @@ from distutils.core import setup
 
 setup(
     name='spectraframe',
-    version='0.1dev',
-    packages=['spectraframe'],
+    version='0.1.2dev',
+    packages=['spectraframe',
+              'spectraframe.spectradataframe',
+              'spectraframe.plotting',
+              'spectraframe.tools'],
     license='MIT License',
+    description='Package to work with sets of spectroscopic data in python.',
+    author='James Haverstick',
+    author_email='jhaverstick99@gmail.com'
 )
